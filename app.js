9ee1119c0fdab6ee6d9100743391ee81f6d0e415
SooeunJang <allmysky7@gmail.com>
2020-02-19 23:04:23 +0900

    Local variables in Pug & Layouts, partials with Pug

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 //import { userRouter } from "./router"; //because it is not export as a default
+import { localsMiddleWare } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -11,12 +12,14 @@ import routes from "./routes";
 
 const app = express();
 
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(helmet());
 app.use(logger("dev"));
 
+app.use(localsMiddleWare);
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
